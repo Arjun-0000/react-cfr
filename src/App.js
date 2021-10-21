54205cb392c166dba5277150ea8f5584850c6813
Arjun-0000 <bboyarjun07@gmail.com>
2021-10-21 08:20:38 +0545

    Create a react app and first publish

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './Component/Navbar';
+import { NavbarCopy } from './Component/NavbarCopy';
+
+function App() {
+  return (
+    <>
+    <h3 className="">Arun </h3>
+    <p>Arjun Shrestha</p>
+    <Navbar />
+    <NavbarCopy />
+    </>
+  );
+}
+
+export default App;
