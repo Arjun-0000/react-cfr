234ea8c6080e503c1a5cfca3479e33db0d4a7b8e
Arjun-0000 <bboyarjun07@gmail.com>
2022-03-12 21:42:40 +0545

    Added button to test Different React Functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import React, { useState } from 'react';
 import './App.css';
 import { Navbar } from './Component/Navbar';
 import Header from './Component/Header';
+import { Button } from 'reactstrap';
+import Structure from './Classes/Structure';
+
 
 function App() {
+
+  const [ showNavbar, setShowNavbar ] = useState(true);
+
+  const fxnShowNavbar = () => {
+    showNavbar ? setShowNavbar(false) : setShowNavbar(true) ;
+  }
+
   return (
-    <>
-    <Navbar />
-    <Header />
-    </>
+    <div>
+      { 
+        showNavbar 
+        ?
+        <div> 
+          <Navbar /> 
+          <Header />
+        </div>
+        :
+        <div>
+          <Structure />
+        </div> 
+      }
+      <Button onClick={fxnShowNavbar}>test change pages</Button>
+    </div>
   );
 }
 
--- src/Component/Description.js
+++ src/Component/Description.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router , Link, Route } from 'react-router-dom';
 import { Button, Col, Container, Row } from 'reactstrap';
+import Background from './Special/Background';
 
 export const Description = () => {
     return (
@@ -12,6 +14,15 @@ export const Description = () => {
                     </Col>
                     <Col>
                         <Button color="primary">Join For Free</Button>
+                    </Col>
+                    <Col xs = "12"> 
+                        <a href="./Special"><Button className="btn btn-secondary">Try JS</Button></a>   
+                        <Router>
+                            <Link to="/Special" className="btn btn-danger">Special</Link>
+                            <Route path="/Special" component={Background} >
+                                
+                            </Route>
+                        </Router>
                     </Col>
                 </Row>
             </Container>
